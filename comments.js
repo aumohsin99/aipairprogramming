170c2128750b823799c1390185ccc3be3654f77b
Ata Ul Mohsin <57590526+aumohsin99@users.noreply.github.com>
2023-08-13 11:07:16 +0000

    comments done

---
+++ comments.js
@@ -0,0 +1,17 @@
+// create web server
+// load express module
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// load body-parser module
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// load express-handlebars module
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
